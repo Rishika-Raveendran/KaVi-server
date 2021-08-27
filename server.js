e571cced899b86910dbbb47308c925d7727e3573
Rishika <rishikarocky14@gmail.com>
2021-08-28 00:54:57 +0530

    .

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ cron.schedule("* * * * *", function () {
 //require route
 app.use("/", router);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('build'));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'build','index.html'));
-//   })
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build','index.html'));
+  })
+}
 
 app.listen(PORT, function () {
   console.log("Server is listening");
